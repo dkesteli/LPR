6d21871ea7c5b149bb7e7ff1a3d5dd813f6613c1
dkesteli <dkesteli@ulb.ac.be>
2015-01-13 10:45:53 +0100

    correction retour de fonction

--- src/exam_q5.c
+++ src/exam_q5.c
@@ -12,12 +12,12 @@
 #include <conio.h>
 #include <ctype.h>
 
-int supprimeNeg(short **v, short taille);
+void supprimeNeg(short **v, short *taille);
 
 int main(int argc, char **argv) {
 
 	short *v, n = 2, i;
-	short newTaille, taille = 5;
+	short taille = 5;
 	if((v = (short *) calloc(taille,sizeof(short)))) {
 		for(i = 0; i < taille; i++) {
 			*(v+i) = n;
@@ -27,17 +27,16 @@ int main(int argc, char **argv) {
 	for(i = 0; i < taille; i++) {
 		printf("%d\n",*(v+i));
 	}
-	newTaille = supprimeNeg(&v,taille);
-	printf("newTaille : %d\n",newTaille);
-	for(i = 0; i < newTaille; i++) {
+	supprimeNeg(&v,&taille);
+	for(i = 0; i < taille; i++) {
 		printf("%d\n",*(v+i));
 	}
 	return 0;
 }
 
-int supprimeNeg(short **vect, short taille) {
+void supprimeNeg(short **vect, short *taille) {
 	short *vreturn = NULL, i, cpt = 0, *v = *vect;
-	for(i = 0; i < taille; i++) {
+	for(i = 0; i < *taille; i++) {
 		if(*(v+i) >= 0) {
 			if(vreturn == NULL) {
 				vreturn = (short *) malloc(sizeof(short));
@@ -52,5 +51,7 @@ int supprimeNeg(short **vect, short taille) {
 		}
 	}
 	*vect = vreturn;
-	return ((cpt < taille) ? cpt:taille);
+	if(cpt < *taille) {
+		*taille = cpt;
+	}
 }
